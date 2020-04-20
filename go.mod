536dc0c5b68f20d915f7cd8a051c746cc307625e
Joaquin Diaz <joacod@gmail.com>
2020-04-20 19:52:06 +0200

    Test added

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module playground
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.6.2
+require (
+	github.com/gin-gonic/gin v1.6.2
+	github.com/stretchr/testify v1.4.0
+)
--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	r := setupRouter()
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	return r
 }
